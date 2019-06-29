8a401d3acd4f376a5320ad478568bfd5efc2e27a
BonnieYu <Bonnie_Yu@wistron.com>
2019-06-29 13:58:39 +0800

    Updates

---
+++ precache-manifest.d8d57820877e4369d138907d9810e003.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef75083804feb4a3715a34cfb1c86ca5",
+    "url": "/more-courses/index.html"
+  },
+  {
+    "revision": "918ce12093f4f919c6af",
+    "url": "/more-courses/static/css/main.0f23716c.chunk.css"
+  },
+  {
+    "revision": "3e4912f26f9d35b6c3e4",
+    "url": "/more-courses/static/js/2.0aceb299.chunk.js"
+  },
+  {
+    "revision": "918ce12093f4f919c6af",
+    "url": "/more-courses/static/js/main.40647560.chunk.js"
+  },
+  {
+    "revision": "644eb67c55a18a3f6697",
+    "url": "/more-courses/static/js/runtime~main.13d835b8.js"
+  }
+]);
